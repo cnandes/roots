bb93dc8dad24575d3dd5b18002c357998c14d748
Thomas Cochrane <thomasrcochrane@icloud.com>
2022-12-17 16:59:52 +1300

    Model validations implemented

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -2,5 +2,5 @@ class Crop < ApplicationRecord
   belongs_to :veggie
   belongs_to :bed
 
-  validates :name, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
