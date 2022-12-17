9e5855d754236a17732a62126e13b49c8be993d6
Claire Fernandes <claire@hotdoc.com.au>
2022-12-17 16:05:18 +1100

    created index page

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -5,6 +5,20 @@ class GardensController < ApplicationController
   end
 
   def create
-    @garden = Garden.new
+    @user = current_user
+    @garden = Garden.new(strong_params)
+    @garden.user = @user
+
+    if @garden.save
+      redirect_to gardens_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:garden).permit(:name)
   end
 end
