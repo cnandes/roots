0c26ae977bd95ba524855e51e8d9336061909129
gurmukhsingh <gurmukhs300@gmail.com>
2022-12-20 15:59:44 +1100

    can update garden

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,23 @@
 class GardensController < ApplicationController
+  before_action :set_garden, only: %i[edit update]
+  def edit
+  end
+
+  def update
+    if @garden.update(garden_params)
+      redirect_to root_path, notice: "NapSpace was successfully update."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name)
+  end
+
+  def set_garden
+    @garden = Garden.find(params[:id])
+  end
 end
