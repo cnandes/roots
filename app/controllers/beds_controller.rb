573d2b6de2f1955d66ffedde07c00cde71762094
Claire Fernandes <claire@hotdoc.com.au>
2023-01-05 20:22:19 +1100

    added bed routes, index method and create method

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -1,2 +1,26 @@
 class BedsController < ApplicationController
+  before_action :set_garden, only: %i[index create]
+  def index
+    @beds = @garden.beds
+  end
+
+  def create
+    @bed = Bed.new(bed_params)
+    @bed.garden = @garden
+    if @bed.save
+      redirect_to garden_path(@garden)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bed_params
+    params.require(:bed).permit(:description, :length, :width)
+  end
+
+  def set_garden
+    @garden = Garden.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :gardens
+  resources :gardens do
+    resources :beds, only: %i[new create index show edit update]
+  end
   resources :veggies, only: %i[new create index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
