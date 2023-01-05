4b1fb10ce4a2d84b328d578bb869c071e6ecd38a
Claire Fernandes <claire@hotdoc.com.au>
2023-01-05 19:28:59 +1100

    added all routes and create method

--- app/controllers/veggies_controller.rb
+++ app/controllers/veggies_controller.rb
@@ -1,2 +1,16 @@
 class VeggiesController < ApplicationController
+  def create
+    @veggie = Veggie.new(veggie_params)
+    if @veggie.save
+      redirect_to veggies_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def veggie_params
+    params.require(:veggie).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :gardens, only: %i[edit update destroy]
+  resources :veggies, only: %i[new create index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
