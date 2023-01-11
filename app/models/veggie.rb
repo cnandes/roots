1ada6a214c0a2199388080bb693a2a29879f3ddc
Claire Fernandes <claire@hotdoc.com.au>
2023-01-12 02:01:08 +1100

    added bed card and logic for displaying crops in the bed

--- app/models/veggie.rb
+++ app/models/veggie.rb
@@ -2,4 +2,10 @@ class Veggie < ApplicationRecord
   has_many :beds, through: :crops
 
   validates :name, presence: true
+
+  # method for front end testing only
+  def emoji
+    emoji_array = %i[🍎 🌽 🥑 🥝 🍌 🥦 🍑 🍅 🍆]
+    return emoji_array.sample
+  end
 end
