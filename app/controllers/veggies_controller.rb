e40d13b5f80f3f115ec9ae83e4a9101a1078dcff
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-01-11 20:39:14 +1100

    added correct redirects to Veggie#create

--- app/controllers/veggies_controller.rb
+++ app/controllers/veggies_controller.rb
@@ -3,10 +3,18 @@ class VeggiesController < ApplicationController
     @veggies = Veggie.all
   end
 
+  def new
+    @veggie = Veggie.new
+  end
+
   def create
     @veggie = Veggie.new(veggie_params)
     if @veggie.save
-      redirect_to :back
+      if params[:garden_id]
+        redirect_to gardens_path(Garden.find(params[:garden_id]))
+      else
+        redirect_to new_veggy_path, notice: "#{@veggie.name} has been added!"
+      end
     else
       # TODO: same as the others - won't render in modal
       # render :new, status: :unprocessable_entity
