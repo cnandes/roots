8557283807b29012b5944847b7be529b019cfb2b
Archana Majumdar <archie.majumdar@gmail.com>
2023-01-12 15:17:47 +1100

    add background to home page

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -1,46 +1,76 @@
-import { Controller } from '@hotwired/stimulus'
-import * as THREE from 'three'
+/* ----------------------- import stimulus and threejs ---------------------- */
 
-// Connects to data-controller="threejs"
+import { Controller } from "@hotwired/stimulus";
+import * as THREE from "three";
+
+/* ------------------ connect to data-controller="threejs" ----------------- */
 export default class extends Controller {
-  connect () {
-    console.log('hello stimulus', this.element)
+  connect() {
+    console.log("hello stimulus", this.element);
 
-    this.scene = new THREE.Scene()
+    /* -------------------------------- constants ------------------------------- */
+    const red = 0xdc143c;
+    const blue = 0x4169e1;
+    const white = 0xfdf5e6;
+    const black = 0x000000;
+    const color2 = new THREE.TextureLoader().load("/assets/grass.png");
+
+    const backgroundText = new THREE.TextureLoader().load();
+
+    /* ------------------------------ scene set up ------------------------------ */
+    this.scene = new THREE.Scene();
+    this.scene.background = color2;
     this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
-    )
-    this.renderer = new THREE.WebGLRenderer()
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(this.renderer.domElement)
+    );
 
-    this.geometry = new THREE.BoxGeometry()
+    this.camera.position.set(0, 0, 5);
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    /* ------------------------------- create cube ------------------------------ */
+    this.geometry = new THREE.BoxGeometry();
     this.material = new THREE.MeshBasicMaterial({
-      color: 0x00ff00,
-      wireframe: true
-    })
-    this.cube = this.createCube(0, 0, 0)
-    this.scene.add(this.cube)
+      color: red,
+      wireframe: false,
+    });
 
-    this.camera.position.set(0, 0, 5)
+    this.cube = this.createCube(0, 0, 0);
+    this.offsetcube = this.createCube(1, 1, -2);
 
-    this.animate()
+    /* -------------------------------- add cube -------------------------------- */
+
+    this.scene.add(this.cube);
+    this.scene.add(this.offsetcube);
+
+    this.animate();
   }
 
-  animate () {
-    requestAnimationFrame(this.animate.bind(this))
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
-    this.renderer.render(this.scene, this.camera)
+  /* -------------------------------- functions ------------------------------- */
+
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    /* --------------------------------- cube 1 --------------------------------- */
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    /* --------------------------------- cube 2 --------------------------------- */
+    this.offsetcube.rotation.x -= 0.01;
+    this.offsetcube.rotation.y -= 0.01;
+
+    /* --------------------------------- render --------------------------------- */
+    this.renderer.render(this.scene, this.camera);
   }
 
-  createCube (x, y, z) {
-    const cube = new THREE.Mesh(this.geometry, this.material)
-    cube.position.set(x, y, z)
-    this.scene.add(cube)
-    return cube
+  createCube(x, y, z) {
+    const cube = new THREE.Mesh(this.geometry, this.material);
+    cube.position.set(x, y, z);
+    this.scene.add(cube);
+    return cube;
   }
 }
