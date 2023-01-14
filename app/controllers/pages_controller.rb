8e5b408c621fe0047339f6860985d0ceff142fa2
Claire Fernandes <claire@hotdoc.com.au>
2023-01-14 15:24:35 +1100

    added dashboard background image and garden card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,8 @@ class PagesController < ApplicationController
 
   def dashboard
     # TO DO: define all of these 🤪
-    # @current_garden =
-    # @upcoming_gardens =
-    # @past_gardens =
+    # @current_garden = [array of crops that are planted: true]
+    # @upcoming_gardens = [array of crops planted: false, plant_date.empty?]
+    # @past_gardens = [array of crops planted: false, plant_date: true]
   end
 end
