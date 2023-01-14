1731a6bec2a0c565bc0529725cea797d1900ccdb
Claire Fernandes <claire@hotdoc.com.au>
2023-01-14 16:56:21 +1100

    updated logic to preveny emojis from overshooting bed on garden index

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -5,4 +5,8 @@ class Bed < ApplicationRecord
   validates :description, presence: true
   validates :length, presence: true, numericality: { greater_than: 0 }
   validates :width, presence: true, numericality: { greater_than: 0 }
+
+  def planted_crops
+    return crops.where(planted: true)
+  end
 end
