832c0f476fad566a6a660c89294e292bee426b19
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-01-14 11:36:39 +1100

    season added to veggie model, validations and forms updated

--- app/models/veggie.rb
+++ app/models/veggie.rb
@@ -2,4 +2,8 @@ class Veggie < ApplicationRecord
   has_many :beds, through: :crops
 
   validates :name, presence: true, uniqueness: true
+  validates :season, presence: true, inclusion: {
+    in: %w[Summer Autumn Winter Spring],
+    message: "%<value> is not a valid season"
+  }
 end
