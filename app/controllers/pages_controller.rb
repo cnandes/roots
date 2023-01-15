2ff9fa3b8fa14af2ceec71185ed888b68a642193
Claire Fernandes <claire@hotdoc.com.au>
2023-01-15 23:32:24 +1100

    updates to dash garden partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
   end
 
   def dashboard
-    # TO DO: define all of these 🤪
-    # @current_garden = [array of crops that are planted: true]
-    # @upcoming_gardens = [array of crops planted: false, plant_date.empty?]
-    # @past_gardens = [array of crops planted: false, plant_date: true]
+    @gardens = current_user.gardens
   end
 end
