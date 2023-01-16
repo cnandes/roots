6637e00f9471bf8fc4b676ae35e3f7642b42f05e
Claire Fernandes <claire@hotdoc.com.au>
2023-01-16 13:59:14 +1100

    added stimulus controller for dashboard filtering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,15 @@ class PagesController < ApplicationController
 
   def dashboard
     @gardens = current_user.gardens
+    # @crops = current_user.crops
+    # @crops_history_date_range = crops_history_date_range(@crops)
   end
+
+  privates
+
+  # def crops_history_date_range(crops)
+  #   first = crops.first.created_at.year
+  #   last = Date.today.year
+  #   return (first..last)
+  # end
 end
