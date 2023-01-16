16272cbf14fd4a58e67c8056e204118af53b1c7a
Claire Fernandes <claire@hotdoc.com.au>
2023-01-16 14:00:45 +1100

    added upcoming season form to dash view

--- app/javascript/controllers/dashboard_filtering_controller.js
+++ app/javascript/controllers/dashboard_filtering_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["year-upcoming", "season-upcoming", "year-past", "season-past"]
   connect() {
     this.element.textContent = "Hello World!"
   }
