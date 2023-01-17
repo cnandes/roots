590a2973d2b11dc61f6a5771652d80c3075acab9
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-01-17 19:56:35 +1100

    validations and strong params updated

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -38,7 +38,7 @@ class BedsController < ApplicationController
   private
 
   def bed_params
-    params.require(:bed).permit(:description, :length, :width)
+    params.require(:bed).permit(:description)
   end
 
   def set_bed
--- app/models/bed.rb
+++ app/models/bed.rb
@@ -3,8 +3,6 @@ class Bed < ApplicationRecord
   has_many :crops, dependent: :destroy
 
   validates :description, presence: true
-  validates :length, presence: true, numericality: { greater_than: 0 }
-  validates :width, presence: true, numericality: { greater_than: 0 }
 
   def planted_crops
     return crops.where(planted: true)
