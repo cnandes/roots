30255500b110d31ee25ea4d817b9d6b35001ceb4
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-01-18 20:10:12 +1100

    Added seasonal veggies

--- db/veggie_names.rb
+++ db/veggie_names.rb
@@ -1,4 +1,4 @@
-VEGE_ARRAY = [
+SUMMER_VEG_ARRAY = [
   "Tomatoes",
   "Capsicums",
   "Peppers",
@@ -12,4 +12,40 @@ VEGE_ARRAY = [
   "Sweet Potatoes",
   "Peas",
   "Tomatillos"
+]
+
+AUTUMN_VEG_ARRAY = [
+  "Beetroot",
+  "Broccoli",
+  "Brussels Sprouts",
+  "Cabbages",
+  "Cauliflower",
+  "Kale",
+  "Lettuce",
+  "Onion",
+  "Spinnach"
+]
+
+WINTER_VEG_ARRAY = [
+  "Turnips",
+  "Radishes",
+  "Garlic",
+  "Butternuts",
+  "Parsnips",
+  "Carrots",
+  "Potatoes",
+  "Pumpkins",
+  "Zucchini",
+  "Asparagus"
+]
+
+SPRING_VEG_ARRAY = [
+  "Artichokes",
+  "Rhubarb",
+  "Scallions",
+  "Watercress",
+  "Leeks",
+  "Zucchinis",
+  "Celery",
+  "Collards"
 ]
