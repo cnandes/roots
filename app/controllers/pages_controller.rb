6a0223d024c37d7cfe2db4c1ac32ffce44b7f296
Claire Fernandes <claire@hotdoc.com.au>
2023-01-18 20:53:50 +1100

    added route, controller action and view for history

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,24 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_gardens, only: %i[plans history]
 
   def home
   end
 
   def plans
-    @gardens = current_user.gardens
+  end
+
+  def history
   end
   # def crops_history_date_range(crops)
   #   first = crops.first.created_at.year
   #   last = Date.today.year
   #   return (first..last)
   # end
+
+  private
+
+  def set_gardens
+    @gardens = current_user.gardens
+  end
 end
