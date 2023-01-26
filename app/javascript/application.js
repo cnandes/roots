be250fea6171073c7ec2ed934d91960b507733ef
Claire Fernandes <claire@hotdoc.com.au>
2023-01-26 20:35:41 +1100

    popovers working now

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
