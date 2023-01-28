e358b3514788d7b51e4e96949886a500acbcb359
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-01-28 11:51:59 +1100

    the fastest working tips non-popover i could make

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Entry point for the build script in your package.json
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
+// window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 import "three"
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
