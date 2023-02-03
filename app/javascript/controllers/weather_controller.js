c93be43034b531dc57a0d250b52e4e9eb1fefdc1
Claire Fernandes <claire@hotdoc.com.au>
2023-02-03 22:05:31 +1100

    controller connected and loading local temp in navbar

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,6 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
+  static targets = ["temp", "icon"]
+  static values = {
+    apiKey: String,
+  }
+
   connect() {
+    console.log("weather controller :)");
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=-37.81271616112838&lon=144.95845581892425&appid=${this.apiKeyValue}`)
+      .then(response => response.json())
+      .then(data => this.displayWeatherData(data));
+  }
+
+  displayWeatherData(data) {
+    console.log(data);
+    console.log(data.weather[0].main)
+    console.log(Math.round((data.main.temp - 273.15)))
+    this.tempTarget.innerHTML = `${Math.round((data.main.temp - 273.15))}°C`;
+    // let date = new Date(Date.now() + data.timezone * 1000);
+    // date = date.toUTCString();
+    // const iconId = data.weather[0].icon;
+    // this.displayTarget.innerHTML = `${data.name}`;
+    // this.iconTarget.src = `https://openweathermap.org/img/w/${iconId}.png`;
+    // this.dateTarget.innerHTML = `${date}`;
   }
 }
