eecc5b189c811abcd7c431b788a73066da239370
Claire Fernandes <claire@hotdoc.com.au>
2023-02-03 22:21:15 +1100

    updated nav item widths to account for temp loading time

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -17,13 +17,8 @@ export default class extends Controller {
   displayWeatherData(data) {
     console.log(data);
     console.log(data.weather[0].main)
-    console.log(Math.round((data.main.temp - 273.15)))
     this.tempTarget.innerHTML = `${Math.round((data.main.temp - 273.15))}°C`;
-    // let date = new Date(Date.now() + data.timezone * 1000);
-    // date = date.toUTCString();
-    // const iconId = data.weather[0].icon;
-    // this.displayTarget.innerHTML = `${data.name}`;
-    // this.iconTarget.src = `https://openweathermap.org/img/w/${iconId}.png`;
-    // this.dateTarget.innerHTML = `${date}`;
+    // complete on another branch, will update background image of avatar nav icon
+    // this.iconTarget.classList.add(data.weather[0].main)
   }
 }
