1223baa06e3a4aea7c80eeb8edb70c1d52cba8af
Claire Fernandes <claire@hotdoc.com.au>
2023-02-03 22:45:11 +1100

    commented out console log of weather data

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -8,15 +8,13 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log("weather controller :)");
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=-37.81271616112838&lon=144.95845581892425&appid=${this.apiKeyValue}`)
       .then(response => response.json())
       .then(data => this.displayWeatherData(data));
   }
 
   displayWeatherData(data) {
-    console.log(data);
-    console.log(data.weather[0].main)
+    // console.log(data);
     this.tempTarget.innerHTML = `<h4>${Math.round((data.main.temp - 273.15))}</h4><h6>°C</h6>`;
     // complete on another branch, will update background image of avatar nav icon
     // this.iconTarget.classList.add(data.weather[0].main)
